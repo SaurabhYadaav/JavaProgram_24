6c5dc03cfe7fb152e2723f8035cc3731aef6f3e0
Saurabh Yadav <saurabhyadav@saurabh-mac.local>
2024-04-11 12:48:04 +0530

    11 APR 2024

---
+++ src/ex_11042024/Hello.java
@@ -0,0 +1,8 @@
+package ex_11042024;
+
+public class Hello
+{
+    public static void main(String[] args) {
+        System.out.println("Hi Intellij");
+    }
+}
---
+++ src/ex_11042024/JavaAdd.java
@@ -0,0 +1,12 @@
+package ex_11042024;
+
+public class JavaAdd
+{
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(3-3);
+        System.out.println(4/2);
+        System.out.println(4%3);
+        System.out.println(4*7);
+    }
+}
