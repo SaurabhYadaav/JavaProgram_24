ea6a6b041f9422887d568e4f7be29c8c07b87194
saurabhyadav <saurabh1996official@gmail.com>
2024-04-18 09:02:54 +0530

    11 APR 2024

---
+++ src/Assignments/Assign_20_001.java
@@ -0,0 +1,10 @@
+package Assignments;
+/*
+Max between 3 Numbers with ? operators.
+Q - int a = 10; System.out.println(++a + a++ + a++);
+Q. - int b = 10; System.out.println(--b + b++ + ++b);
+Value of x,y, z - double - result.
+
+ */
+public class Assign_20_001 {
+}
